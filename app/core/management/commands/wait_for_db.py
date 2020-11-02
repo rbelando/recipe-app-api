65f7ad3e1b61257fd0c25c946336c69fef10e8a4
rbelando <ruben.belando.sanchez@gmail.com>
2020-11-02 23:00:28 +0100

    configured postgresDB

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,21 @@
+import time
+from typing import Any
+
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to pause execution unti database is available"""
+
+    def handle(self, *args: Any, **options: Any) -> None:
+        self.stdout.write("Waiting for database...")
+        db_con = None
+        while not db_con:
+            try:
+                db_con = connections["default"]
+            except OperationalError:
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS("Database available!"))
