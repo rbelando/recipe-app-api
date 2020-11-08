5686e233a12527242b5aed91346697020b21a421
rbelando <ruben.belando.sanchez@gmail.com>
2020-11-08 18:28:30 +0100

    added list ingredients feature.

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
 from recipe import views
 
 router = DefaultRouter()
 router.register("tags", views.TagViewSet)
+router.register("ingredients", views.IngredientViewSet)
 
 app_name = "recipe"
 
